943af5e7c542c9667f7793f417c458503ccecda3
ak4rnis <48867159+ak4rnis@users.noreply.github.com>
2023-11-08 20:27:17 -0300

    commit 08 11

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('dashboard/', views.dashboard, name="dashboard"),
     path('', views.dashboard, name="dashboard"),
-    
+    path('my_orders/', views.my_orders, name='my_orders'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import redirect, render
-from accounts.forms import RegistrationForm
-from accounts.models import Account
+from django.shortcuts import get_object_or_404, redirect, render
+from accounts.forms import RegistrationForm, UserForm, UserProfileForm
+from accounts.models import Account, UserProfile
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
@@ -12,6 +12,8 @@ from django.core.mail import EmailMessage
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
 import requests
+
+from orders.models import Order
 
 # Create your views here.
 def register(request):
@@ -29,6 +31,10 @@ def register(request):
             user.phone_number = phone_number
             user.is_active = True
             user.save()
+            profile = UserProfile()
+            profile.user_id = user.id # type: ignore
+            profile.profile_picture = 'default/default-user.png' # type: ignore
+            profile.save()
 
             messages.success(request, 'Se registro el usuario existosamente')
             return redirect('login')
@@ -119,6 +125,69 @@ def activate(request, uidb64, token):
          messages.error(request, 'La activacion es invalida')
          return redirect('register')
     
+
 @login_required(login_url='login')
 def dashboard(request):
-     return render(request, 'accounts/dashboard.html')
+     orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+     orders_count = orders.count()
+     context = {
+         'orders_count': orders_count,
+     }
+     return render(request, 'accounts/dashboard.html', context)
+
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+    context = {
+        'orders': orders
+    }
+    return render(request, 'accounts/my_orders.html', context)
+
+@login_required(login_url='login')
+def edit_profile(request):
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Su informacion fue guardada con exito')
+            return redirect('edit_profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=userprofile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'userprofile': userprofile,
+    }
+
+    return render(request, 'accounts/edit_profile.html', context)
+
+
+@login_required(login_url='login')
+def change_password(request):
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+
+        user = Account.objects.get(username__exact=request.user.username)
+
+        if new_password == confirm_password:
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+
+                messages.success(request, 'El Password se actualizo exitosamente')
+                return redirect('change_password')
+            else:
+                messages.error(request, 'Por favor ingrese un password valido')
+                return redirect('change_password')
+        else:
+            messages.error(request, 'El password no coincide con la confirmacion de password')
+            return redirect('change_password')
+
+    return render(request, 'accounts/change_password.html')
